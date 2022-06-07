43ca6db228e63166331ee3be651d5f9e56a120e3
feo23dev <f.efe.okyar@gmail.com>
2022-06-08 02:03:46 +0300

    radar
    Showing  go on radar

---
+++ ObseverPattern/Assets/Radar/Event.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Event", menuName = "Game Event", order = 52)]
+
+public class Event : ScriptableObject
+{
+    private List<EventListener> elisteners = new List<EventListener>();
+
+    public void Register(EventListener listener)
+    {
+        elisteners.Add(listener);
+    }
+
+    public void Unregister(EventListener listener)
+    {
+        elisteners.Remove(listener);    
+    }
+
+    public void Occured(GameObject go)
+    {
+        for(int i = 0; i< elisteners.Count; i++)
+        {
+            elisteners[i].OnEventOccurs(go);
+        }
+    }
+
+
+
+
+
+
+    
+}
